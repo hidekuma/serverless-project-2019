780bfea8f31e0bab3f3c6d84c83998a90719c6d8
hidekuma <d.hidekuma@gmail.com>
2019-10-28 15:53:11 +0900

    Updated test/*

---
+++ test/index.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+    //$('#fixed-tab-2').on('click', function(){
+        $.ajax({
+            url: 'https://epn2pajpf4.execute-api.ap-northeast-2.amazonaws.com/dev/conference?user_phone=*',
+            method: 'get',
+            success: function(r){
+                var html = '';
+                r['items'].forEach(function(item) {
+                    html += '<div  class="mdl-cell mdl-cell--4-col">'
+                    html += '<div class="history-card-wide mdl-card mdl-shadow--2dp">'
+                    html += '<div class="mdl-card__title" style="background: url(\'https://fcsw-2019.s3.ap-northeast-2.amazonaws.com/qrcodes/'+item['user_phone']+'/qrcode.jpg\') center / cover;">'
+                    //html += '<div>'
+                    //html += 'Serverless Web project'
+                    html += '<h2 class="mdl-card__title-text">'+item['user_name']+'</h2>'
+                    html += '</div>'
+                    html += '<div class="mdl-card__supporting-text">'
+                    html += item['company_name']
+                    html += ' / '
+                    html += item['user_name']
+                    html += ' / '
+                    html += item['user_phone']
+                    html += '</div>'
+                    html += '<div class="mdl-card__menu">'
+                    html += '<button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">'
+                    html += '<i class="material-icons">share</i>'
+                    html += '</button>'
+                    html += '</div>'
+                    html += '</div>'
+                    html += '</div>'
+                })
+
+                 $('#history').html(html);
+            },
+            fail: function(err){
+                console.log('failed', err);
+            },
+            complete: function(r){
+                console.log('completed', r);
+            }
+        //});
+    });
+    $('#submitButton').on('click', function(e){
+        var user_name = $('#user_name').val();
+        var user_phone = $('#user_phone').val();
+        var company_name = $('#company_name').val();
+        $.ajax({
+            url: 'https://epn2pajpf4.execute-api.ap-northeast-2.amazonaws.com/dev/conference',
+            method: 'post',
+            datatype: 'json',
+            async: true,
+            data:JSON.stringify({
+                user_name: user_name,
+                user_phone: user_phone,
+                company_name: company_name
+            }),
+            beforeSend: function(){
+                $('#p2').show();
+            },
+            success: function(r){
+                console.log('success', r);
+            },
+            fail: function(err){
+                console.log('failed', err);
+            },
+            complete: function(r){
+                console.log('completed', r);
+                $('#p2').hide();
+                location.reload();
+            }
+        });
+    });
+})
