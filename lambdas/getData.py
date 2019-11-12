215f1cfb7c2807e933ee13e1b01540a799ad3b4a
hidekuma <d.hidekuma@gmail.com>
2019-11-12 15:02:00 +0900

    Updated lambda get data

--- lambdas/getData.py
+++ lambdas/getData.py
@@ -1,17 +1,26 @@
+import json
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
 
 def lambda_handler(event, context):
+    # TODO implement
     dynamo = boto3.resource('dynamodb')
     table = dynamo.Table('test')
+
 
     if event['user_id'] == '*':
         items = table.scan()
     else:
-        fe = Key('user_id').eq(event['user_id']) & Key('type').eq(event['type'])
+        type = event.get('type', None)
+        if type:
+            fe = Key('user_id').eq(event['user_id']) & Key('type').eq(event['type'])
+        else:
+            fe = Key('user_id').eq(event['user_id'])
+    
         items = table.query(
             KeyConditionExpression=fe
         )
+    
     return {
         'statusCode': 200,
         'items': items['Items']
